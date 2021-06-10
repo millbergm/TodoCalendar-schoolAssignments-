6d6c1ec34c63ad56b469deef2800be0ae8d83aa0
millbergm <83matmil@skola.boras.se>
2021-06-10 10:58:17 +0200

    add new todo to array

---
+++ wwwroot/js/todo.js
@@ -0,0 +1,13 @@
+class TodoItem {
+    constructor(title, startDate, stopDate) {
+        this.title = title;
+        this.startDate = startDate;
+        this.stopDate = stopDate;
+    }
+}
+
+var allTodos = new Array();
+
+function addNewTodo(TodoItem) {
+    allTodos.push(this.TodoItem);
+}
