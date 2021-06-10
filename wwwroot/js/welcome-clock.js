0e1298d861fa0115c25c5181718fe499ce5a19c8
Andreas Gunnarsson <andreas.gunnarsson@gmail.com>
2021-06-10 10:06:22 +0200

    - Fixed code structure.

--- wwwroot/js/welcome-clock.js
+++ wwwroot/js/welcome-clock.js
@@ -3,24 +3,24 @@ window.addEventListener('DOMContentLoaded', main);
 function main() {
     getCurrentDateTime();
     window.setInterval(getCurrentDateTime, 1000);
+}
 
-    function getCurrentDateTime() {
-        const timeElement = document.getElementById('welcome-time');
-        const dateElement = document.getElementById('welcome-date');
+function getCurrentDateTime() {
+    const timeElement = document.getElementById('welcome-time');
+    const dateElement = document.getElementById('welcome-date');
 
-        const currentDate = new Date();
+    const currentDate = new Date();
 
-        const hours = addZeroBefore(currentDate.getHours());
-        const minutes = addZeroBefore(currentDate.getMinutes());
-        const seconds = addZeroBefore(currentDate.getSeconds());
+    const hours = addZeroBefore(currentDate.getHours());
+    const minutes = addZeroBefore(currentDate.getMinutes());
+    const seconds = addZeroBefore(currentDate.getSeconds());
 
-        timeElement.innerText = hours + ' : ' + minutes + ' : ' + seconds;
+    timeElement.innerText = hours + ' : ' + minutes + ' : ' + seconds;
 
-        const date = currentDate.toLocaleString('sv-SE', { month: 'long', year: 'numeric', day: 'numeric'});
-        const weekday = currentDate.toLocaleString('sv-SE', { weekday: 'long' });
+    const date = currentDate.toLocaleString('sv-SE', { month: 'long', year: 'numeric', day: 'numeric'});
+    const weekday = currentDate.toLocaleString('sv-SE', { weekday: 'long' });
 
-        dateElement.innerText = weekday + ', ' + date;
-    }
+    dateElement.innerText = weekday + ', ' + date;
 }
 
 function addZeroBefore(n) {
