068315f382eddd0c5fd760827f3dea585a072a62
Andreas Gunnarsson <andreas.gunnarsson@gmail.com>
2021-06-10 09:52:39 +0200

    - Modified welcome-clock to be more responsive. - Uses const instead of let.

--- wwwroot/js/welcome-clock.js
+++ wwwroot/js/welcome-clock.js
@@ -1,23 +1,28 @@
-window.setInterval(getCurrentDateTime, 1000);
+window.addEventListener('DOMContentLoaded', main);
 
-function addZeroBefore(n) {
-  return (n < 10 ? '0' : '') + n;
+function main() {
+    getCurrentDateTime();
+    window.setInterval(getCurrentDateTime, 1000);
+
+    function getCurrentDateTime() {
+        const timeElement = document.getElementById('welcome-time');
+        const dateElement = document.getElementById('welcome-date');
+
+        const currentDate = new Date();
+
+        const hours = addZeroBefore(currentDate.getHours());
+        const minutes = addZeroBefore(currentDate.getMinutes());
+        const seconds = addZeroBefore(currentDate.getSeconds());
+
+        timeElement.innerText = hours + ' : ' + minutes + ' : ' + seconds;
+
+        const date = currentDate.toLocaleString('sv-SE', { month: 'long', year: 'numeric', day: 'numeric'});
+        const weekday = currentDate.toLocaleString('sv-SE', { weekday: 'long' });
+
+        dateElement.innerText = weekday + ', ' + date;
+    }
 }
 
-function getCurrentDateTime() {
-    const timeElement = document.getElementById('welcome-time');
-    const dateElement = document.getElementById('welcome-date');
-
-    const currentDate = new Date();
-
-    let hours = addZeroBefore(currentDate.getHours());
-    let minutes = addZeroBefore(currentDate.getMinutes());
-    let seconds = addZeroBefore(currentDate.getSeconds());
-
-    timeElement.innerText = hours + ' : ' + minutes + ' : ' + seconds;
-
-    let date = currentDate.toLocaleString('sv-SE', { month: 'long', year: 'numeric', day: 'numeric'});
-    let weekday = currentDate.toLocaleString('sv-SE', { weekday: 'long' });
-
-    dateElement.innerText = weekday + ', ' + date;
+function addZeroBefore(n) {
+    return (n < 10 ? '0' : '') + n;
 }
