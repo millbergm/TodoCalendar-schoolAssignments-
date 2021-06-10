68e98eb4de8839a01d1b570d4837e9367c6ee501
millbergm <83matmil@skola.boras.se>
2021-06-10 11:46:56 +0200

    added function getTodoByDate

--- wwwroot/js/todo.js
+++ wwwroot/js/todo.js
@@ -1,8 +1,14 @@
+
 class TodoItem {
+    /**
+     * @param {Date} startDate
+     * @param {Date} stopDate
+     */
     constructor(title, startDate, stopDate) {
         this.title = title;
         this.startDate = startDate;
         this.stopDate = stopDate;
+        this.isDone = false;
     }
 }
 
@@ -14,4 +20,15 @@ function addNewTodo(TodoItem) {
 
 function deleteTodoByIndex(i) {
     allTodos.splice(i, 1);
+}
+
+/**
+ * 
+ * @param {Date} date 
+ * @returns 
+ */
+function getTodosByDate(date) {
+    const day = date.getDate()
+    const todosByDate = allTodos.find(TodoItem => TodoItem.startDate.getDate == day);
+    return todosByDate;
 }
