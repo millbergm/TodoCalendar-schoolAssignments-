2704cc122abe0e7a16d3b73fa68327cb56c6fd49
Joacim Olsson <Joacim.olsson95@gmail.com>
2021-06-10 15:12:57 +0200

    setupCalender

--- wwwroot/js/Calendar.js
+++ wwwroot/js/Calendar.js
@@ -4,25 +4,55 @@ function setup() {
   const currentDate = new Date();
   setupCalender(currentDate.getFullYear(), currentDate.getMonth() + 1);
 }
+
 function setupCalender(year, month) {
   getDaysInfoAsync(year, month, (responseText) => {
     const response = JSON.parse(responseText);
     const days = response.dagar;
-    console.log(days);
     for (let index = 1; index < days[0]["dag i vecka"]; index++) {
-      console.log(index, days[0]["dag i vecka"]);
       createEmtyDay();
     }
+    days.forEach((day) => buldADay(day));
     for (let index = 7; index > days[days.length - 1]["dag i vecka"]; index--) {
-      console.log(index, days[days.length - 1]["dag i vecka"]);
       createEmtyDay();
     }
   });
 }
 
-function buldADay(dayinfo)
-{
+/*
+<div class="daycontaner col" data-calenderDate="">
+    <div class="row">
+        <div class="col">
+            <div class="day-Nr">1</div>
+            <div class="todo-nr">0</div>
+        </div>
+        <div class="day-red-day col">sveriges nationaldag</div>
+     </div>
+</div>
+*/
+function buldADay(dayinfo) {
+  const datum = dayinfo.datum.split("-");
 
+  const daycontaner = document
+    .querySelector(".daycontaner.temp")
+    .cloneNode(true);
+  daycontaner.classList.remove("temp");
+
+  daycontaner.querySelector(".day-Nr").innerText = datum[datum.length - 1];
+  daycontaner.querySelector(".todo-nr").innerText = 0; // call a function to get this
+  daycontaner.dataset.calenderdate = dayinfo["datum"];
+  if (dayinfo["röd dag"] === "Ja") {
+    daycontaner.classList.add("red");
+  }
+
+  if (dayinfo["helgdag"]) {
+    daycontaner.querySelector(".day-red-day").innerText = dayinfo["helgdag"];
+  } else {
+    daycontaner.querySelector(".day-red-day").remove();
+  }
+
+  const calendarDays = document.getElementById("calendar-Days");
+  calendarDays.append(daycontaner);
 }
 
 function createEmtyDay() {
