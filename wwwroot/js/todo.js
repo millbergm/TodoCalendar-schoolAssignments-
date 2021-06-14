19a1f94b429e879855d72b5d4d60722010e67e26
millbergm <83matmil@skola.boras.se>
2021-06-14 12:56:55 +0200

    handleFormSubmit v:1

--- wwwroot/js/todo.js
+++ wwwroot/js/todo.js
@@ -15,6 +15,11 @@ class TodoItem {
 
 var allTodos = new Array();
 
+function addFormEventListener() {
+    let form = document.querySelector('form');
+    form.addEventListener('submit', handleFormSubmit);
+}
+
 function addNewTodo(TodoItem) {
     allTodos.push(TodoItem);
 }
@@ -42,4 +47,42 @@ function getTodosByDate(date) {
     const day = date.getDate()
     const todosByDate = allTodos.find(TodoItem => TodoItem.startDate.getDate == day);
     return todosByDate;
+}
+
+function initTodos() {
+    const todoString = localStorage.getItem('allTodos');
+    allTodos = JSON.parse(todoString || "[]");
+    renderTodoList();
+}
+
+function saveTodosToLocalStorage() {
+    localStorage.setItem('allTodos', JSON.stringify(allTodos));
+}
+
+/**
+ * @param {Event} event
+ */
+ function handleFormSubmit(event) {
+     event.preventDefault();
+     var todoTitle = document.getElementById('todoTitle');
+     var todoInfo = document.getElementById('todoInfo');
+     var startDate = document.getElementById('startDate');
+     var stopDate = document.getElementById('stopDate');
+     var TodoItem = {title: todoTitle.value, info: todoInfo.value, startDate: startDate, stopDate: stopDate.value, isDone: false}
+     allTodos.push(TodoItem);
+     // input.value = "";
+     
+     saveTodosToLocalStorage();
+    //  renderTodoList();
+}
+
+function renderTodoList() {
+    const ul = document.querySelector('ul');
+    ul.innerText = "";
+
+    for (var todo of allTodos) {
+        const li = document.createElement('li');
+        li.innerText = todo;
+        ul.append(li);
+    }
 }
