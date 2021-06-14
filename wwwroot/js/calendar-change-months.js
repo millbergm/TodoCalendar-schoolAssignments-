f26b61eafe355e1ad5574a29d66d79a3ad04cc3e
Joacim Olsson <Joacim.olsson95@gmail.com>
2021-06-14 11:24:57 +0200

    added changeMonths

--- wwwroot/js/calendar-change-months.js
+++ wwwroot/js/calendar-change-months.js
@@ -1,25 +1,36 @@
-const monthBackButton = document.getElementById('month-back');
-monthBackButton.addEventListener('click', monthBack);
+const monthBackButton = document.getElementById("month-back");
+monthBackButton.addEventListener("click", monthBack);
 
-const monthForwardButton = document.getElementById('month-forward');
-monthForwardButton.addEventListener('click', monthForward);
+const monthForwardButton = document.getElementById("month-forward");
+monthForwardButton.addEventListener("click", monthForward);
 
 const monthSpan = document.getElementById("current-month");
 let currentDateChangeMonths;
 
 function initMonthButtons(date) {
-    currentDateChangeMonths = date;
-    monthSpan.innerText = currentDateChangeMonths.toLocaleString('sv-SE', { month: 'long', year: 'numeric' });
+  currentDateChangeMonths = date;
+  monthSpan.innerText = currentDateChangeMonths.toLocaleString("sv-SE", {
+    month: "long",
+    year: "numeric",
+  });
 }
 
 function monthBack() {
-    currentDateChangeMonths.setMonth(currentDateChangeMonths.getMonth() - 1);
-    monthSpan.innerText = currentDateChangeMonths.toLocaleString('sv-SE', { month: 'long', year: 'numeric' });
-    setupCalender(currentDateChangeMonths.getFullYear(), currentDateChangeMonths.getMonth() + 1);
+  changeMonths(-1);
 }
 
 function monthForward() {
-    currentDateChangeMonths.setMonth(currentDateChangeMonths.getMonth() + 1);
-    monthSpan.innerText = currentDateChangeMonths.toLocaleString('sv-SE', { month: 'long', year: 'numeric' });
-    setupCalender(currentDateChangeMonths.getFullYear(), currentDateChangeMonths.getMonth() + 1);
+  changeMonths(1);
+}
+
+function changeMonths(change) {
+  currentDateChangeMonths.setMonth(currentDateChangeMonths.getMonth() + change);
+  monthSpan.innerText = currentDateChangeMonths.toLocaleString("sv-SE", {
+    month: "long",
+    year: "numeric",
+  });
+  setupCalender(
+    currentDateChangeMonths.getFullYear(),
+    currentDateChangeMonths.getMonth() + 1
+  );
 }
